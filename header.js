97b3e1a3f17503ab2d516568e19484a362776767
danstuff <raydongames@gmail.com>
2015-01-10 22:01:57 -0500

    Getting Started
    The basics of the site, all pasted in.

---
+++ header.js
@@ -0,0 +1,11 @@
+document.write('\
+    <img src="header.png" usemap="#headermap">\
+\
+	<map name="headermap">\
+		<area shape="rect" coords="0,    18, 460,  97" alt="DanStuff"  href="index.htm">\
+		<area shape="rect" coords="0,   117, 128, 155" alt="Home"      href="index.htm">\
+		<area shape="rect" coords="181, 117, 338, 155" alt="Games"     href="games.htm">\
+		<area shape="rect" coords="392, 117, 521, 155" alt="More"      href="more.htm">\
+		<area shape="rect" coords="570, 117, 724, 155" alt="About"     href="about.htm">\
+	</map>\
+');
---
+++ recentgames.js
@@ -0,0 +1,6 @@
+document.write('\
+    <h1>Recently Completed Games</h1>\
+\
+	<a href="http://bit.ly/1G76BxG"><img src="thumbs/spookybeat.jpg" alt="Spooky Beat!"></a>\
+	<a href="http://bit.ly/1G76BxG"><img src="thumbs/spacecrusade.jpg" alt="Space Crusade"></a>\
+');
