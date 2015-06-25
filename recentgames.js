adaf5b88e8330ccb6f0ee04d160f6cf0fb34b4f0
danstuff <raydongames@gmail.com>
2015-06-24 20:22:57 -0400

    General Improvements

--- recentgames.js
+++ recentgames.js
@@ -1,6 +1,4 @@
 document.write('\
-    <h1>Recent Games</h1>\
-\
 	<a href="games.htm"><img src="thumbs/inprogress.jpg" alt="Dweller"></a>\
 	<a href="games.htm"><img src="thumbs/spookybeat.jpg" alt="Spooky Beat!"></a>\
 	<a href="games.htm"><img src="thumbs/spacecrusade.jpg" alt="Space Crusade"></a>\
--- recenttweets.js
+++ recenttweets.js
@@ -1,6 +1,4 @@
 document.write('\
-<div id=”twitter_update_list”>\
-  <script type=”text/javascript” src=”http://twitter.com/javascripts/blogger.js”></script>\
-  <script type=”text/javascript” src=”http://twitter.com/statuses/user_timeline/thetoastiest.json?callback=twitterCallback2&count=1″></script>\
-</div>\
+<a class="twitter-timeline" href="https://twitter.com/TheToastiest" data-widget-id="598662859678089216">Tweets by @TheToastiest</a>\
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>\
 ');
