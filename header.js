31be66be8d68a9df0ab5666ddb45e40f431f2622
danstuff <raydongames@gmail.com>
2015-08-12 21:49:09 -0400

    Game Download Pages
    Fleshed out the individual game pages with pertinent information and
    added download links. Also did some reorganization of resource files.

--- header.js
+++ header.js
@@ -18,9 +18,10 @@ document.write('\
 \
 	<map name="headermap">\
 		<area shape="rect" coords="37,   64, 460, 97 " alt="DanStuff"  href="index.htm">\
-		<area shape="rect" coords="18,  119, 124, 145" alt="Home"      href="#home">\
-		<area shape="rect" coords="189, 119, 317, 145" alt="Games"     href="#games">\
-		<area shape="rect" coords="382, 119, 516, 145" alt="Videos"    href="#videos">\
-		<area shape="rect" coords="580, 119, 709, 145" alt="About"     href="#about">\
+		<area shape="rect" coords="18,  119, 124, 145" alt="Home"      href="index.htm#home">\
+		<area shape="rect" coords="189, 119, 317, 145" alt="Games"     href="index.htm#games">\
+		<area shape="rect" coords="382, 119, 516, 145" alt="Videos"    href="index.htm#videos">\
+		<area shape="rect" coords="580, 119, 709, 145" alt="About"     href="index.htm#about">\
 	</map>\
+	<br><br><br>\
 ');
--- medialinks.js
+++ medialinks.js
@@ -1,4 +1,5 @@
 document.write('\
-	<a href="https://www.twitter.com/TheToastiest"><img src="medialinks/twitter.png" alt="Twitter"></a>\
-	<a href="https://www.youtube.com/roosterteeth"><img src="medialinks/youtube.png" alt="Twitter"></a>\
+	<br><br><br>\
+	<a href="https://www.twitter.com/TheToastiest"><img src="twitter.png" alt="Twitter"></a>\
+	<a href="https://www.youtube.com/roosterteeth"><img src="youtube.png" alt="Twitter"></a>\
 	');
