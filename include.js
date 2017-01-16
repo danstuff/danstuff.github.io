e89cb1ff23e288498cb55e35790a315244a3f300
danstuff <danyost23@gmail.com>
2017-01-16 16:59:36 -0500

    Job Descriptions and URL Tags
    Added ability to customize how the website looks by adding tags at the
    end of the URL.

--- include.js
+++ include.js
@@ -17,4 +17,45 @@ function openTab(evt, name){
 	window.scrollTo(0,0);
 }
 
+function hasParameter(name, url){
+	//populate url
+	if(!url) url = window.location.href;
+	
+	//search for everything after a ?name= or &name=
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+	
+	var results = regex.exec(url);	
+	
+	//return false if not found, otherwise true
+	if(!results) 
+		return false;
+		
+	return true;
+}
+
 document.getElementById("default").click();
+
+//cut certain elements based on URL tags
+var elem;
+
+if(hasParameter('software_only')){
+	elem = document.getElementById("tab_theater");
+	elem.parentNode.removeChild(elem);
+	
+	elem = document.getElementById("res_theater");
+	elem.parentNode.removeChild(elem);
+}
+if(hasParameter('theater_only')){
+	elem = document.getElementById("tab_software");
+	elem.parentNode.removeChild(elem);
+	
+	elem = document.getElementById("res_software");
+	elem.parentNode.removeChild(elem);
+}
+if(hasParameter('no_tabs')){
+	var elem = document.getElementById("tab_software");
+	elem.parentNode.removeChild(elem);
+	var elem = document.getElementById("tab_theater");
+	elem.parentNode.removeChild(elem);
+}
