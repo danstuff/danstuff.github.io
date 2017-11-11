0930c3dd2c039cbb3afea5cd5abdf74585ade851
danstuff <danyost23@gmail.com>
2017-11-10 23:02:44 -0500

    I don't know why I made these changes
    Forgot, oops

--- include.js
+++ include.js
@@ -1,3 +1,38 @@
+var slide_index = 0;
+
+function slideshow(index){
+	var slides;
+	
+	slides =  document.getElementsByClassName("slide");
+	
+	if(!slides) return;
+	
+	if(index >= slides.length){
+		slide_index = 0;
+	}else if(index < 0){
+		slide_index = slides.length-1;
+	}
+	
+	index = slide_index;
+	
+	for(i = 0; i < slides.length; i++){
+		if(i != index){
+			slides[i].style.display = "none";
+		}else{
+			slides[i].style.display = "block";
+		}
+	}
+}
+
+function slideshowLeft(){
+	slide_index--;
+	slideshow(slide_index);
+}
+function slideshowRight(){
+	slide_index++;
+	slideshow(slide_index);
+}
+
 function openTab(evt, name){
 	var i, tabcontent, tablinks;
 	
@@ -25,53 +60,50 @@ function hasParameter(name, url){
 	return (url.indexOf(name) > -1);
 }
 
-function printPage(){
+function removeCat(name){
 	var elem;
+	
+	elem = document.getElementById("tab_"+name);
+	if(elem)	elem.parentNode.removeChild(elem);
+	
+	elem = document.getElementById("res_"+name);
+	if(elem)	elem.parentNode.removeChild(elem);
+}
 
-	elem = document.getElementById("header");
-	elem.parentNode.removeChild(elem);
-	
-	elem = document.getElementById("footer");
-	elem.parentNode.removeChild(elem);
+function setSize(selector, font_size, line_height){
+	var elems = document.querySelectorAll(selector);
+	for(var i in elems){
+		if(elems.hasOwnProperty(i)){
+			elems[i].style.fontSize = font_size;
+			elems[i].style.lineHeight = line_height;
+		}
+	}			
+}
 
-	elem = document.getElementById("resume");
-	elem.style.paddingTop = 0;
+function printPage(){
+	document.getElementById("default").click();
 	
-	elem = document.getElementById("header_print");
-	elem.innerHTML = "<h6>danyost23@gmail.com <br>\
-			908-451-2213 <br>\
-			danstuff.github.io</h6>\
-			<h4>DANIEL YOST</h4>\
-			<h5>Student - Westfield, NJ</h5><br><br>";
+	//remove header and footer
+	var header = document.getElementById("header");
+	header.parentNode.removeChild(header);
+	
+	var footer = document.getElementById("footer");
+	footer.parentNode.removeChild(footer);
+
+	var resume = document.getElementById("resume");
+	resume.style.paddingTop = 0;
+		
+	var elem = document.getElementById("header_print");
+	elem.innerHTML = "<h3 style=\"float:right;\">danyost23@gmail.com <br><br>\
+			908-451-2213 <br><br>\
+			danstuff.github.io</h3>\
+			<br><h1>DANIEL YOST</h1>\
+			<h2 style=\"padding:0;\">Student - Westfield, NJ</h2><br>";
 			
-	elem = document.querySelectorAll("p");
-	for(var i in elem){
-		if(elem.hasOwnProperty(i)){
-			elem[i].style.fontSize = "13px";
-			elem[i].style.lineHeight = "1.5em";
-		}
-	}		
-			
-	elem = document.querySelectorAll("div.tabcontent h1");
-	for(var i in elem){
-		if(elem.hasOwnProperty(i)){
-			elem[i].style.fontSize = "16px";
-		}
-	}
-	
-	elem = document.querySelectorAll("div.tabcontent h2");
-	for(var i in elem){
-		if(elem.hasOwnProperty(i)){
-			elem[i].style.fontSize = "15px";
-		}
-	}
-	
-	elem = document.querySelectorAll("div.tabcontent h3");
-	for(var i in elem){
-		if(elem.hasOwnProperty(i)){
-			elem[i].style.fontSize = "13px";
-		}
-	}
+	setSize("p",  "0.75em", "1.25em");
+	setSize("h1", "1em", "0.5em");
+	setSize("h2", "1em", "0.5em");
+	setSize("h3", "1em", "0.5em");
 	
 	window.print();
 	
@@ -80,31 +112,10 @@ function printPage(){
 
 document.getElementById("default").click();
 
-//cut certain elements based on URL tags
-var elem;
+//cut elements based on URL tags
+if(hasParameter('nsoft'))	removeCat("software");
+if(hasParameter('nthea'))	removeCat("theater");
+if(hasParameter('nvolu'))	removeCat("volunteer");
 
-if(hasParameter('nsoft')){
-	elem = document.getElementById("tab_software");
-	elem.parentNode.removeChild(elem);
-	
-	elem = document.getElementById("res_software");
-	elem.parentNode.removeChild(elem);
-}
-if(hasParameter('nthea')){
-	elem = document.getElementById("tab_theater");
-	elem.parentNode.removeChild(elem);
-	
-	elem = document.getElementById("res_theater");
-	elem.parentNode.removeChild(elem);
-}
-if(hasParameter('nvolu')){
-	elem = document.getElementById("res_volunteer");
-	elem.parentNode.removeChild(elem);
-}
-if(hasParameter('ntabs')){
-	elem = document.getElementById("tab_software");
-	elem.parentNode.removeChild(elem);
-	
-	elem = document.getElementById("tab_theater");
-	elem.parentNode.removeChild(elem);
-}
+//set up slideshow
+slideshow(slide_index);
