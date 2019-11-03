b16abf58ecbedb0f45a30595cd7d812a18610b52
Dan <danyost23@gmail.com>
2019-11-02 20:03:00 -0400

    Linked forms to database

--- serve.py
+++ serve.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 
-from flask_sqlalchemy import create_engine
-from flask_sqlalchemy import Column, String
-from flask_sqlalchemy.ext.declarative import declarative_base
+from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
 from wtforms import TextField, TextAreaField, SubmitField
@@ -11,18 +9,23 @@ from wtforms.validators import DataRequired, Email
 app = Flask(__name__)
 app.secret_key = 'devKey16821'
 
-Base = declarative_base()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/main.db'
 
-class ContactMessage(Base):
-    __tablename__ = "ContactMessages"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    email = Column(String)
-    message = Column(String)
+db = SQLAlchemy(app)
+
+class ContactMessage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    message = db.Column(db.String(512), nullable=False)
+
+    def __repr__(self):
+        return '<Msg %r %r %r>' % (self.name, self.email, self.message)
 
 class ContactForm(FlaskForm):
     name = TextField("Name", [DataRequired("Please Enter Your Name.")])
-    email = TextField("Email", [Email("Please Enter a Valid Email Address."), DataRequired("Please Enter an Email Address.")])
+    email = TextField("Email", [Email("Please Enter a Valid Email Address."),
+        DataRequired("Please Enter an Email Address.")])
     message = TextAreaField("Message", [DataRequired("Please Enter a Message.")])
     submit = SubmitField("Submit")
 
@@ -32,14 +35,19 @@ class ContactForm(FlaskForm):
 def show_index():
     form=ContactForm()
 
-    print(form.name.data)
-
     if request.method == 'GET':
         return render_template("index.html", form=form)
     elif request.method == 'POST':
         if form.validate() == False:
             return render_template("index.html", form=form)
         else:
+            msg = ContactMessage(name = form.name.data,
+                email = form.email.data,
+                message = form.message.data)
+
+            db.session.add(msg)
+            db.session.commit()
+
             return render_template("recieved.html")
 
 if __name__ == "__main__":
