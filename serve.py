465ca1da5534dfe9db90db75afbd4dc49e994b18
Daniel Yost <danyost23@gmail.com>
2020-07-30 16:03:02 -0400

    Finished draft of daily update function, ready to debug

--- serve.py
+++ serve.py
@@ -8,7 +8,7 @@ from flask_wtf import FlaskForm
 from wtforms import TextField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
 
-from auth import MailUsername, MailPassword, MyEmail, SecretKey, NYTKey, GuaKey APKey, OWMKey
+from auth import MailUsername, MailPassword, MyEmail, SecretKey, NYTKey, OWMKey
 
 app = Flask(__name__)
 app.secret_key = SecretKey
@@ -23,11 +23,13 @@ app.config['MAIL_PASSWORD'] = MailPassword
 
 sender_name = "Danstuff Mail Bot"
 
-weather_fmt = "{} - {}% chance of rain.<br><br>"
-article_fmt = "<b>{}</b><br>{}<br><a href=\"{}\">Read More</a><br><br>"
+weather_fmt = "{} - {} degrees, {}<br><br>"
+weather_locs = ( "Poughkeepise,NY", "Westfield,NJ", "Great Barrington,MA" )
 
-weather_locs = { "Poughkeepise,NY", "Westfield,NJ", "Great Barrington,MA" }
-articles_per_site = 3
+article_fmt = "<b>{}</b><br>{}<br><a href=\"{}\">Read More</a><br><br>"
+article_categories = (("world", 2), ("us", 1), ("politics", 1),
+                      ("technology", 4), ("science", 3), ("arts", 4),
+                      ("food", 2), ("movies", 2))
 
 my_email = MyEmail
 mail = Mail(app)
@@ -71,18 +73,27 @@ def daily_update():
     content = ""
     
     for loc in weather_locs:
-        #TODO fetch chance of rain for given location
-        chance = 0
-        content = content + weather_fmt.format(loc, chance)
+        #fetch chance of rain for given location
+        r = request.get("api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+                        .format(loc, OWMKey)).json()
+
+        temp = r["main"]["temp"]
+        wthr = r["weather"]["description"]
         
-    for i in range(0, articles_per_site):
+        content = content + weather_fmt.format(loc, temp, wthr)
+        
+    for cat in article_categories:
         #TODO fetch article title, content, and URL
-        title = "Article Title"
-        body = "Article body"
-        aurl = "https://www.google.com/"
+        r = request.get("https://api.nytimes.com/svc/topstories/v2/{}.json?api-key={}"
+                        .format(cat[0], NYTKey)).json()
+                        
+        title = r["title"]
+        body = r["abstract"]
+        aurl = r["url"]
+        
         content = content + article_fmt.format(title, body, aurl)
         
-    email = Message("Here's your daily update", sender = sender_name)
+    email = Message("Good morning! Here's your daily update", sender = sender_name)
     email.add_recipient(my_email)
     email.html = content
     
